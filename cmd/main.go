16e2b04f912ef113001deb67b24a1bc9310042fc
mhidate <ayot427@gmail.com>
2025-03-10 16:46:41 +0800

    pertama viber

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"socia/config"
+	"socia/internal/http/routes"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	err := config.ConnectDB()
-	if err != nil {
-		log.Fatalf("Gagal terhubung ke database: %v", err)
-	}
-	defer config.CloseDB()
-	fmt.Println("Aplikasi sedang berjalan.....")
+	app := fiber.New()
+	config.ConnectDB()
+
+	routes.AuthRoutes(app)
+	routes.PhotoRoutes(app)
+
+	app.Listen(":3000")
 }
