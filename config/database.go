8ec37c9a15aa6ecb2d3172db2ffabe8188ae8201
mhidate <ayot427@gmail.com>
2025-03-11 17:14:40 +0800

    merapikan kode database.go

--- config/database.go
+++ config/database.go
@@ -10,21 +10,6 @@ import (
 
 var DB *sql.DB
 
-// func LoadEnv() {
-// 	// Cari path ke root proyek (assume eksekusi dari `cmd/main.go`)
-// 	rootPath, err := filepath.Abs("../") // Kembali ke root proyek
-// 	if err != nil {
-// 		log.Fatalf("Gagal mendapatkan path root proyek: %v", err)
-// 	}
-
-// 	envPath := filepath.Join(rootPath, ".env") // Path lengkap ke .env
-// 	// Load file .env dari root proyek
-// 	err = godotenv.Load(envPath)
-// 	if err != nil {
-// 		log.Println("Peringatan: Tidak dapat memuat file .env, menggunakan environment variable default")
-// 	}
-// }
-
 func ConnectDB() error {
 	// LoadEnv()
 	connStr := os.Getenv("DATABASE_URL") // Ambil dari environment variable
@@ -43,7 +28,6 @@ func ConnectDB() error {
 	}
 
 	DB = db
-	fmt.Println("Berhasil terhubung ke database")
 	return nil
 }
 
